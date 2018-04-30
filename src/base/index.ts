b55b20052405b06f6cb731fd43c86efdeab3c939
WinUP <l.gholoi@outlook.com>
2018-04-30 13:03:44 -0400

    Bug fix.

--- src/base/index.ts
+++ src/base/index.ts
@@ -1,5 +1,6 @@
 export * from './Resource.listener';
 export * from './Message.listener';
 export * from './State.listener';
+export * from './Cache.listener';
 export * from './BaseComponent';
 export * from './AutoRegister';
